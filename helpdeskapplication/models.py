f778e0a071d60df2aca44f89d4fb1eb1a7621006
Jason Cooke <jasonpcooke@gmail.com>
2023-12-14 21:30:16 +0000

    Amended code structure to make more use of  django's rest API. implemented a login page.

--- helpdeskapplication/models.py
+++ helpdeskapplication/models.py
@@ -1,16 +1,40 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+# Person Super User
+class PersonManager(BaseUserManager):
+    def create_person(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        person = self.model(email=email, **extra_fields)
+        person.set_password(password)
+        person.save(using=self._db)
+        return person
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_person(email, password, **extra_fields)
+
 
 # Person
-class Person(models.Model):
+class Person(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True, null=False)
     password = models.CharField(max_length=128, null=False)
     firstname = models.CharField(max_length=50, null=False)
     surname = models.CharField(max_length=50, null=False)
 
-    def __str__(self):
-        return f"{self.user_id} {self.email} {self.password} {self.firstname} {self.surname}"
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = PersonManager()
+
+    USERNAME_FIELD = 'email'
 
 # Asset
 class Asset(models.Model):
